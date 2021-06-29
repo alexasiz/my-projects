188598c474a4d5e066f6d2d5573236fa428c4390
alexasiz <alexasiz2008@gmail.com>
2021-06-29 15:49:23 +0300

    It is updated 29062021

--- python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
+++ python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -3,12 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def head():
+    return "I like Flask!"
 
 @app.route('/second')
 def second():
-    return "Bize Her Yer Ankara"
+    return "This is the second page"
 
 @app.route('/third/subthird')
 def third():
@@ -20,4 +20,4 @@ def forth(id):
 
 if __name__=="__main__":
     app.run(host='0.0.0.0', port=80)
-    #app.run(debug=True)
+    # app.run(debug=True)
--- python/hands-on/flask-03-handling-routes-and-if-for/Flask_if_for_structure/app.py
+++ python/hands-on/flask-03-handling-routes-and-if-for/Flask_if_for_structure/app.py
@@ -1,20 +1,19 @@
-# Import Flask modules
 from flask import Flask, render_template
-# Create an object named app 
-app = Flask(__name__)
-# Create a function named head which shows the massage as "This is my first conditions experience" in `index.html` 
-# and assign to the route of ('/')
+
+app = Flask (__name__)
+
 @app.route('/')
 def head():
-    first = 'This is my first conditions experience'
-    return render_template('index.html', message = first)
-# Create a function named header which prints the items one by one in `body.html` 
-# and assign to the route of ('/')
-@app.route('/vincenzo')
+    first="This is my first condition experience"
+    return render_template("index.html",  message = False )
+
+
+@app.route('/serdar')
 def header():
-    names = ["Ümit", "Berk", "Fatih", "Hayko", "Asım"]
-    return render_template('body.html', object = names)
-# run this app in debug mode on your local.
-if __name__ == '__main__':
-    #app.run(debug=True)
+    name = ["Serdar", "Fatih", "Ali", "Fatma", "Mostafa"]
+    return render_template("body.html", object = name)
+
+
+if __name__=="__main__":
+    # app.run(debug=True)
     app.run(host='0.0.0.0', port=80)
